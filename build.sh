ad9db878b408f8b80c8724d3778438e0f750d3bf
Mirko <mirko@cloudera.com>
2015-09-25 17:14:39 +0200

    Created the second parcel for a Triple Collector Service based on Fuseki2.3

--- build.sh
+++
@@ -1,7 +0,0 @@
-#mvn -Phadoop_2 clean compile install assembly:single
-#ecl
-
-ssh training@54.195.198.28 sudo mkdir /home/training/etosha
-ssh training@54.195.198.28 sudo mkdir /home/training/etosha/bin
-
-
