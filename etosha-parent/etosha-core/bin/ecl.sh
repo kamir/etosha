25df87f352f07fd3fe8d5cbe8061a82787840941
Mirko <mirko.kaempf@gmail.com>
2016-10-02 18:18:14 +0200

    .....

--- etosha-parent/etosha-core/bin/ecl.sh
+++ etosha-parent/etosha-core/bin/ecl.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-
-# Install the script in /usr/sbin
-#
-# cd /usr/sbin
-# sudo ln -s /home/training/workspace/EtoshaCore/bin/ecl.sh ecl
-#
-
-ETOSHA_PATH=/home/cloudera/workspace/etosha/etosha-parent/etosha-core/target/
-UBER_JAR=etosha-core-0.9.0-SNAPSHOT-jar-with-dependencies-filtered.jar
-
+ 
 MAIN=org.etosha.cmd.EtoshaContextLogger
 
-echo "> MAIN:  $MAIN"
-echo "> arg1:  $1"
-echo "> arg2:  $2"
-
-hadoop jar $ETOSHA_PATH/$UBER_JAR $MAIN $1 $2 
+echo "> MAIN-Class :  $MAIN"
+echo "> JAVA_HOME  :  $JAVA_HOME"
+echo "> ARGS       :  [$1, $2]"
+hadoop jar /home/cloudera/workspace/etosha/etosha-parent/etosha-core/target/etosha-core-0.9.0-SNAPSHOT-jar-with-dependencies-filtered.jar $1 $2
