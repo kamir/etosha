c64549b7996c1119b81b14f7bf55398efb13b606
Mirko <mirko.kaempf@gmail.com>
2016-10-02 18:16:51 +0200

    .....

--- etosha-parent/bin/ecl.sh
+++ etosha-parent/bin/ecl.sh
@@ -8,9 +8,9 @@
 
 MAIN=org.etosha.cmd.EtoshaContextLogger
 
-echo "> MAIN-Class:  $MAIN"
-echo "> arg1:  $1"
-echo "> arg2:  $2"
+echo "> MAIN-Class :  $MAIN"
+echo "> JAVA_HOME  :  $JAVA_HOME"
+echo "> ARGS       :  [$1, $2]"
+hadoop jar /home/cloudera/workspace/etosha/etosha-parent/etosha-core/target/etosha-core-0.9.0-SNAPSHOT-jar-with-dependencies-filtered.jar $1 $2
 
-sudo hadoop jar /home/cloudera/workspace/etosha/etosha-parent/etosha-core/target/etosha-core-0.9.0-SNAPSHOT-jar-with-dependencies-filtered.jar $1 $2
 
--- etosha-parent/bin/tests.sh
+++
@@ -1,2 +0,0 @@
-ecl list
-ecl inspectSEQ /user/training/testdata/corpus.compare/corpus.SEOKanzler_DE_31.01.2014.seq
