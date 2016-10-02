72854d8403fd37d8805314a3645e4e9b6f482bed
Mirko <mirko.kaempf@gmail.com>
2016-10-02 17:31:53 +0200

    .........

--- etosha-parent/bin/ecl.sh
+++ etosha-parent/bin/ecl.sh
@@ -6,12 +6,11 @@
 # sudo ln -s /home/training/workspace/etosha/etosha-parent/bin/ecl.sh ecl
 # sudo chmod 777 ecl
 
-MAIN=org.etosha.tools.admin.ContextLoggerTool
+MAIN=org.etosha.cmd.EtoshaContextLogger
 
 echo "> MAIN-Class:  $MAIN"
 echo "> arg1:  $1"
 echo "> arg2:  $2"
-echo ">"
 
-sudo hadoop jar /GITHUB/ETOSHA.WS/etosha/etosha-parent/etosha-core/target/etosha-core-0.9.0-SNAPSHOT-jar-with-dependencies.jar $1 $2
+sudo hadoop jar /home/cloudera/workspace/etosha/etosha-parent/etosha-core/target/etosha-core-0.9.0-SNAPSHOT-jar-with-dependencies-filtered2.jar $1 $2
 
--- etosha-parent/etosha-core/bin/ecl.sh
+++ etosha-parent/etosha-core/bin/ecl.sh
@@ -6,7 +6,7 @@
 # sudo ln -s /home/training/workspace/EtoshaCore/bin/ecl.sh ecl
 #
 
-ETOSHA_PATH=/GITHUB/ETOSHA.WS/etosha/etosha-parent/etosha-core/target/
+ETOSHA_PATH=/home/cloudera/workspace/etosha/etosha-parent/etosha-core/target/
 UBER_JAR=etosha-core-0.9.0-SNAPSHOT-shaded.jar
 
 MAIN=org.etosha.cmd.EtoshaContextLogger
@@ -14,6 +14,5 @@ MAIN=org.etosha.cmd.EtoshaContextLogger
 echo "> MAIN:  $MAIN"
 echo "> arg1:  $1"
 echo "> arg2:  $2"
-echo ">"
 
 hadoop jar $ETOSHA_PATH/$UBER_JAR $MAIN $1 $2 
