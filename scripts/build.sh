f5faa7d52ade125d21c92897a39700653630b30e
Mirko <mirko.kaempf@gmail.com>
2016-10-02 18:01:24 +0200

    ...

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,18 +13,20 @@ cd ..
 cd etosha-parent
 mvn clean compile install package assembly:single
 
+cp ./../etc/etosha-site.xml /home/$USER/etc/etosha/etosha-site.xml
+cp ./../etc/etosha-site.xml /etc/etosha/etosha-site.xml
+
+cd /usr/sbin
+sudo ln -s /home/cloudera/workspace/etosha/etosha-parent/bin/ecl.sh ecl
+sudo chmod 777 ecl
 
 #-------------------------------------------------------------------------------
 # deploy the Etosha CLI ...
 #-------------------------------------------------------------------------------
-ssh $USER@WSHOST sudo mkdir /home/$USER/etosha
-ssh $USER@WSHOST sudo mkdir /home/$USER/etosha/bin
-ssh $USER@WSHOST sudo mkdir /etc/etosha
+#ssh $USER@WSHOST sudo mkdir /home/$USER/etc
+#ssh $USER@WSHOST sudo mkdir /home/$USER/etc/etosha 
+#ssh $USER@WSHOST sudo mkdir /etc/etosha
 
-# scp etosha-site.xml
-# scp UBER_JAR
-# scp script
- 
 
 
 
