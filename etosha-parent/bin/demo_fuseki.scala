89abd63c9f06f35636bd8b1c36d54277c4b7eefd
Mirko <mirko.kaempf@gmail.com>
2017-04-26 15:43:53 +0200

    modified scala demo

--- etosha-parent/bin/demo_fuseki.scala
+++ etosha-parent/bin/demo_fuseki.scala
@@ -2,6 +2,9 @@ import embedded.FusekiStore;
 
 val store = new FusekiStore();
 
+// WE GET SOME Method not found errors => FIX DEPENDENCIES
+
+
 //store.init( "./s1", "data", 3031, "/GITHUB/FusekiCloud/fuseki-parcel/___WORKINGDATA/fuseki/scripts/main/data/model.ttl" );
 //store.addContent( store.getContent("/GITHUB/FusekiCloud/fuseki-parcel/___WORKINGDATA/fuseki/scripts/main/data/partitions/model.ttl") );
 
